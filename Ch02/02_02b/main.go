6ec8ef1dc26691b7ba446813f84a34436ac5b4e8
Jay <1292428+JayOkey@users.noreply.github.com>
2025-01-22 10:45:13 +0000

    chapter one

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,16 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aValue := 42
 
-	fmt.Println("Hello from Go!")
+	fmt.Println(str1, str2, str3)
+	stringLength, error := fmt.Println("The value is ", aValue)
+	if error == nil {
+		fmt.Println(stringLength)
+	}
+	fmt.Printf("The string lenght is %d\n", stringLength)
+	fmt.Printf("The value has a type of %T\n", stringLength)
 }
