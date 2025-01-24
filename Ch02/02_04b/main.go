0277970dda49c92f38ea8331ac64d3fed24c7475
Jay <1292428+JayOkey@users.noreply.github.com>
2025-01-24 11:07:11 +0000

    Chapter 2 4 complete

--- Ch02/02_04b/main.go
+++ Ch02/02_04b/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Math")
+	i1, i2, i3 := 1, 2, 3
+	intSum := i1 + i2 + i3
+	fmt.Println("Integer sum:", intSum)
+
+	f1, f2, f3 := 2.3, 2.44, 3.44
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum:", floatSum)
 }
